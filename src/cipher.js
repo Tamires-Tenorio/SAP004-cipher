6ea1125c54b1050799153a950c207d20d48f7b32
Tamires-Tenorio <tamirestenorio03@gmail.com>
2020-04-09 21:45:13 -0300

    Primeiro commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,33 @@
 const cipher = {
-  // ...
-};
+  encode: function cencode(valorDeslocamento, palavraDigitada) {
 
+    let tamanhoDaPalavra=palavraDigitada.length;  
+    let resultado="";
+    
+    for(let contador=0; contador<tamanhoDaPalavra;contador++){
+     let novaLetra=(palavraDigitada.charCodeAt(contador)-65+valorDeslocamento)%26+65
+     
+
+resultado+=String.fromCharCode(novaLetra);
+  
+    }
+    
+  return resultado    
+
+},  
+  decode: function decode(valorDeslocamento, palavraDigitada){
+    let tamanhoDaPalavra=palavraDigitada.length;  
+    let resultado="";
+
+    for(let contador=0; contador<tamanhoDaPalavra; contador++){
+      let nova=(palavraDigitada.charCodeAt(contador)-90+valorDeslocamento)%26+90
+      
+      resultado+=String.fromCharCode(nova);
+      
+      }
+      
+      return resultado;
+      }
+
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import cipher from './cipher.js';
 
+    
+    
+    const botaoCodificar=document.getElementById("botaoCodificar");
+    const botaoDecodificar=document.getElementById("botaoDecodificar");
+    
+    function printaResultadoCodificar(){
+        let valorDeslocamento = Number(document.getElementById("valorDeslocamento").value);
+        let palavraDigitada = document.getElementById("palavraDigitada").value.toUpperCase();
+
+        document.getElementById("resultado").innerHTML= cipher.encode(valorDeslocamento, palavraDigitada);
+    console.log(cipher.encode(valorDeslocamento, palavraDigitada));
+    }
+    
+    botaoCodificar.addEventListener("click", printaResultadoCodificar);
+   
+
+    function printaResultadoDecodificar(){
+        let valorDeslocamento = -Number(document.getElementById("valorDeslocamento").value);
+        let palavraDigitada = document.getElementById("palavraDigitada").value.toUpperCase();
+
+        document.getElementById("resultado").innerHTML= cipher.decode(valorDeslocamento, palavraDigitada);
+    console.log(cipher.decode(valorDeslocamento, palavraDigitada));
+    }
+    
+    botaoDecodificar.addEventListener("click", printaResultadoDecodificar);
+
+
 console.log(cipher);
